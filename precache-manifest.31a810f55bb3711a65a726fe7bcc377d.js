43c34d048bef96711c1d42499067e9e2cdf4441f
Irma Ivanova <nowa.mir@yandex.ru>
2019-10-20 18:04:29 +0300

    Updates

---
+++ precache-manifest.31a810f55bb3711a65a726fe7bcc377d.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d16ed0e88f2ef17af6bce89c312111",
+    "url": "//irmaivanova.github.io/stripe.github.io/index.html"
+  },
+  {
+    "revision": "ad8fc6a789def64c18f5",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/css/2.222921b5.chunk.css"
+  },
+  {
+    "revision": "c6377f355c62d99d1b31",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/css/main.216aa4c4.chunk.css"
+  },
+  {
+    "revision": "ad8fc6a789def64c18f5",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/js/2.c5cc4c0a.chunk.js"
+  },
+  {
+    "revision": "c6377f355c62d99d1b31",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/js/main.74ef83c1.chunk.js"
+  },
+  {
+    "revision": "3bd54da64500b89c2c41",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/js/runtime-main.baa9fe9e.js"
+  },
+  {
+    "revision": "933738347aa7b9d347d63adb219e9466",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/Payments.93373834.svg"
+  },
+  {
+    "revision": "f9c97b1a44bbee38cf9abee8ca46a536",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/ShoppingBag.f9c97b1a.svg"
+  },
+  {
+    "revision": "a9737d726e4f797df709617c4d38b1fc",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/alipay.a9737d72.svg"
+  },
+  {
+    "revision": "e9c62536bd648ac7f0ef909052b5a53a",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/amazon.e9c62536.svg"
+  },
+  {
+    "revision": "80dbd4908b944048e68a6551cd2a5542",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/billing.80dbd490.svg"
+  },
+  {
+    "revision": "4d9a720347c96110598ab1582506786c",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/block.4d9a7203.svg"
+  },
+  {
+    "revision": "40794c404452c6eb55e1a5a7a4401647",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/booking_com.40794c40.svg"
+  },
+  {
+    "revision": "8fc1450bda66a7dbf8873fc4496d98c2",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/browser.8fc1450b.svg"
+  },
+  {
+    "revision": "62a6cbb3ddad80029e57162b1a1b2972",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/card.62a6cbb3.svg"
+  },
+  {
+    "revision": "f9aa3aee095b4fd3a74048071b8d3d86",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/card1.f9aa3aee.png"
+  },
+  {
+    "revision": "33a747e774b1509f6c0e42153b27ba49",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/charity_water.33a747e7.svg"
+  },
+  {
+    "revision": "e0a8e915908d7aa8ef721c0716ecb3ab",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/charity_water.e0a8e915.jpg"
+  },
+  {
+    "revision": "b4ce1595c4a987d7800dadc4b47bffc1",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/conect.b4ce1595.svg"
+  },
+  {
+    "revision": "38835bee91ae2f132478ae6e244206b0",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/conerdark.38835bee.svg"
+  },
+  {
+    "revision": "02b9e23479a4eb1736d6cd74dc7e0f39",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/docusign.02b9e234.svg"
+  },
+  {
+    "revision": "2583ed38ab30adeaccc16e496fac637c",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/download.2583ed38.svg"
+  },
+  {
+    "revision": "7cf82584b91cc34fca8a032a2df57319",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/eu.7cf82584.svg"
+  },
+  {
+    "revision": "bc2da8687cdb08f19b3100ca01b029ff",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/glob.bc2da868.svg"
+  },
+  {
+    "revision": "714c5bb2a24b61df218ef7059ec38589",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/global.714c5bb2.svg"
+  },
+  {
+    "revision": "39d96922c40ae984d630fd2e09bdd953",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/glossier.39d96922.jpg"
+  },
+  {
+    "revision": "3e806128d359bef3f9a7513570ac30cd",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/glossier.3e806128.svg"
+  },
+  {
+    "revision": "505de2598633d179026ddcff4ffe30d6",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/grafic.505de259.svg"
+  },
+  {
+    "revision": "22531494751738cf1d180a17a02860df",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/ideal.22531494.svg"
+  },
+  {
+    "revision": "602098214cb5a086f1129c03366fab19",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/instacart.60209821.svg"
+  },
+  {
+    "revision": "a4f9ca197818e6f76697036e4fade69f",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/leguage.a4f9ca19.svg"
+  },
+  {
+    "revision": "f0dc6df6dc4dc670eabc7d8ea1b6225b",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/luft.f0dc6df6.svg"
+  },
+  {
+    "revision": "8d5f3cb178cd5705a1bc3658d75f9195",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/lyft.8d5f3cb1.jpg"
+  },
+  {
+    "revision": "c16fdcdd8d83725ae02ffdbc72830e2e",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/lyft.c16fdcdd.svg"
+  },
+  {
+    "revision": "137fee208083a5873fd6cf758ba66f21",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/missguided.137fee20.jpg"
+  },
+  {
+    "revision": "4630cad5e76b740296f3eead149b7b12",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/missguided.4630cad5.svg"
+  },
+  {
+    "revision": "0e326a80ed9dbe829fd511cf4297a0b4",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/partnerships.0e326a80.svg"
+  },
+  {
+    "revision": "2d9e754c2ef2c8b043c67f6e8c2df791",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/pci.2d9e754c.svg"
+  },
+  {
+    "revision": "9801312d34e6cc1d5324e8f7d18806f1",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/peleton.9801312d.svg"
+  },
+  {
+    "revision": "94fb87e2bc83ddec6d45c41b8c49aa0c",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/phone.94fb87e2.svg"
+  },
+  {
+    "revision": "83045c00ac2cce405a6166081988cfae",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/reliability.83045c00.svg"
+  },
+  {
+    "revision": "5ef7b42e82fb5be8d86160849f102e74",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/sepa.5ef7b42e.svg"
+  },
+  {
+    "revision": "40a37a8da8a390c04903aaa9a83049af",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/shopify.40a37a8d.svg"
+  },
+  {
+    "revision": "085e48b79c42c874582d5f33d2795718",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/slack.085e48b7.svg"
+  },
+  {
+    "revision": "aa5aeedda25c44f69615af3c0ad28ca9",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/slack.aa5aeedd.jpg"
+  },
+  {
+    "revision": "508e775badb6823a66f3d41ea563a4b6",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/soc.508e775b.svg"
+  },
+  {
+    "revision": "f5bff725d9c9ba2653786e57a7fb4fed",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/tablet.f5bff725.svg"
+  },
+  {
+    "revision": "153b5562f5380cd8369c92edfd62d296",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/unicef.153b5562.svg"
+  },
+  {
+    "revision": "83484684eaa236ee53369836afa49a2d",
+    "url": "//irmaivanova.github.io/stripe.github.io/static/media/us.83484684.svg"
+  }
+]);
